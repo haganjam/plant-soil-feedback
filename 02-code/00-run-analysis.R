c1c35e79b6d1a8ea91f32921475760e68b8e4f27
haganjam <james_hagan@outlook.com>
2025-09-10 15:07:29 +0200

    run the analysis to produce the expeirment 2 results for the initial drafting of the publication

--- 02-code/00-run-analysis.R
+++ 02-code/00-run-analysis.R
@@ -15,7 +15,7 @@ species_list <- c("Erigeron_canadensis",
 for (sp in species_list) {
   
   # create an output file name
-  output_file <- paste0("report-", sp, ".html")
+  output_file <- paste0("report-exp1-", sp, ".html")
   
   # render the relevant quarto document
   quarto_render(
@@ -32,11 +32,55 @@ for (sp in species_list) {
   
 }
 
+# additional files
+file_name <- "05-experiment-1_files"
+
 # remove the created files if present
-if ("05-experiment-1_files" %in% list.files("02-code")) {
+if (file_name %in% list.files("02-code")) {
   # delete the file
-  file_delete(here::here("02-code/quarto-renders", "05-experiment-1_files"))  
+  if (file_name %in% list.files()) {
+    file_delete(here::here("02-code/quarto-renders", file_name)) 
+  }
   # move file
-  file_move("02-code/05-experiment-1_files", here::here("02-code/quarto-renders", "05-experiment-1_files"))
+  file_move(file.path("02-code", file_name), here::here("02-code/quarto-renders", file_name))
 }
+
+# experiment 2 analysis
+for (sp in species_list) {
+  
+  # create an output file name
+  output_file <- paste0("report-exp2-", sp, ".html")
+  
+  # render the relevant quarto document
+  quarto_render(
+    input = "02-code/06-experiment-2.qmd",
+    output_file = output_file,
+    execute_params = list(
+      invasive_species = sp,
+      exclude_highest_N = FALSE
+    )
+  )
+  
+  # move file
+  file_move(output_file, here::here("02-code/quarto-renders", output_file))
+  
+}
+
+# additional files
+file_name <- "06-experiment-2_files"
+
+# remove the created files if present
+if (file_name %in% list.files("02-code")) {
+  # delete file
+  if (file_name %in% list.files()) {
+    file_delete(here::here("02-code/quarto-renders", file_name)) 
+  } 
+  # move file
+  file_move(file.path("02-code", file_name), here::here("02-code/quarto-renders", file_name))
+}
+
+
+
+
+
 
